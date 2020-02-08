05201887fdb528005b1c675f144dc6ae57d78eff
Johnny-Go <49007932+Johnny-Go@users.noreply.github.com>
2020-02-08 11:44:58 -0800

    Fix: Running prettier

--- src/Components/FileImportButton.tsx
+++ src/Components/FileImportButton.tsx
@@ -1,31 +1,20 @@
-import React from "react";
-import Button from "@material-ui/core/Button";
+import React from 'react'
+import Button from '@material-ui/core/Button'
 
 type FileImportButtonProps = {
-  id: string;
-  children: React.ReactNode;
-  accept: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-};
+  id: string
+  accept: string
+  children: React.ReactNode
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+}
 
-export const FileImportButton = ({
-  id,
-  children,
-  accept,
-  onChange
-}: FileImportButtonProps) => (
+export const FileImportButton = ({ id, accept, children, onChange }: FileImportButtonProps) => (
   <React.Fragment>
-    <input
-      accept={accept}
-      id={id}
-      style={{ display: "none" }}
-      type="file"
-      onChange={onChange}
-    />
+    <input id={id} accept={accept} onChange={onChange} style={{ display: 'none' }} type="file" />
     <label htmlFor={id}>
-      <Button component="span" variant="contained" color="primary">
+      <Button color="primary" component="span" variant="contained">
         {children}
       </Button>
     </label>
   </React.Fragment>
-);
+)

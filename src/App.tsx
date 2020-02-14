79c5069a16b19332b5f22d36e5ab9b75d6271ee9
Johnny-Go <49007932+Johnny-Go@users.noreply.github.com>
2020-02-13 17:21:12 -0800

    Feat: Read split file (#6)
    * Feat: Read split file
    
    * Fix: Simpifying code with null conditional operators
    
    * Fix: PR comments, and more simplifing with null conditionals

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,46 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import FileImportButton from './Components/FileImportButton'
+import TextField from '@material-ui/core/TextField'
+import xml2js from 'xml2js'
+import Attempt from './types'
 
 const App = () => {
-  function handleFile() {
-    console.log('test')
+  const [attemptHistory, setAttemptHistory] = React.useState<Attempt[]>([])
+
+  function handleFile(e: React.ChangeEvent<HTMLInputElement>) {
+    if (e?.target?.files?.[0]) {
+      const file = e.target.files[0]
+
+      const reader = new FileReader()
+      reader.onload = () => {
+        const xml = reader.result?.toString()
+
+        if (xml) {
+          xml2js.parseString(xml, { trim: true }, async function(err, result) {
+            //todo: display error nicely in UI
+            if (err) {
+              console.log(err)
+            }
+
+            await getAttemptHistory(result)
+          })
+        }
+      }
+      reader.readAsText(file)
+    }
+  }
+
+  async function getAttemptHistory(xml: any) {
+    let atmptHistory: Attempt[] = []
+
+    xml?.Run?.AttemptHistory?.[0]?.Attempt?.forEach((attempt: any) => {
+      if (attempt?.$?.id && attempt?.RealTime?.[0]) {
+        atmptHistory.push({ x: attempt.$.id, y: attempt.RealTime[0] })
+      }
+    })
+
+    setAttemptHistory(atmptHistory)
   }
 
   return (
@@ -26,6 +62,13 @@ const App = () => {
         <FileImportButton id="import-split-button" accept=".lss" onChange={handleFile}>
           Import LiveSplit File
         </FileImportButton>
+        <br />
+        <TextField
+          multiline
+          value={attemptHistory.map((a: Attempt) => `x:${a.x}, y:${a.y}`).join('\n')}
+          style={{ width: '75%' }}
+          variant="outlined"
+        />
       </header>
     </div>
   )
